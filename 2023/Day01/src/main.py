a3f3b10b2683e56cfaea6260cf0072a09b19b66e
Alessio Bardelli <alessio.bardelli@intecs.it>
2023-12-24 12:35:16 +0100

    Aggiunta la soluzione per il giorno 1.

--- 2023/Day01/src/main.py
+++ 2023/Day01/src/main.py
@@ -5,4 +5,32 @@ from re import findall
 if __name__ == "__main__" and len(argv) >= 2:
     with open(argv[1]) as file:
         lines = file.readlines()
-        print("Part One:", sum([int(val[0] + val[-1]) for val in ["".join(findall(r"\d", line)) for line in lines]]))
+        print("Part One:", sum([int(val[0] + val[-1]) for val in [findall(r"\d", line) for line in lines]]))
+        _sum = 0
+        convert = {
+            "1": "one",
+            "2": "two",
+            "3": "three",
+            "4": "four",
+            "5": "five",
+            "6": "six",
+            "7": "seven",
+            "8": "eight",
+            "9": "nine",
+        }
+        for line in lines:
+            min_idxs: list[tuple[str, int]] = []
+            max_idxs: list[tuple[str, int]] = []
+            for key in convert:
+                line = line.replace(key, convert[key])
+            for key in convert:
+                result = line.find(convert[key])
+                if result != -1:
+                    min_idxs.append((key, result))
+                result = line.rfind(convert[key])
+                if result != -1:
+                    max_idxs.append((key, result))
+            _min = min(min_idxs, key=lambda x: x[1])
+            _max = max(max_idxs, key=lambda x: x[1])
+            _sum += int(_min[0] + _max[0])
+        print("Part Two:", _sum)
