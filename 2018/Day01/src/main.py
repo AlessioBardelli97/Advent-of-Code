0481dcdd36b743606290bf0c6aa84a81089780c7
Alessio Bardelli <alessio.bardelli@intecs.it>
2025-01-07 12:31:03 +0100

    Migliorata la soluzione del giorno 01 dell'evento 2018.

--- 2018/Day01/src/main.py
+++ 2018/Day01/src/main.py
@@ -3,11 +3,9 @@ from sys import argv
 
 if __name__ == "__main__" and len(argv) >= 2:
   with open(argv[1]) as file:
-    old_frequency, frequencies = [], []
-    current_frequency, i = 0, 0
-    for line in file.readlines():
-      frequencies.append(int(line))
-    while len(old_frequency) < 2 or current_frequency not in old_frequency:
+    old_frequency, current_frequency, i = [], 0, 0
+    frequencies = [int(line) for line in file.readlines()]
+    while current_frequency not in old_frequency:
       old_frequency.append(current_frequency)
       current_frequency += frequencies[i]
       i = (i + 1) % len(frequencies)
